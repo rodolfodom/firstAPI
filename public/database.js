9235359c64427dfb3f2094a923a093d8a181d685
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-03-15 08:08:57 -0600

    I changed database string connection to enviroment variable

--- public/database.js
+++ public/database.js
@@ -2,12 +2,9 @@ const MongoClient = require("mongodb").MongoClient;
 
 async function connect() {
   try {
-    const client = await MongoClient.connect(
-      "mongodb+srv://generalRank:Rufetty2%2E@cluster0.37ghr.mongodb.net/firstapi?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-      }
-    );
+    const client = await MongoClient.connect(process.env.DATABASE_STRING, {
+      useUnifiedTopology: true,
+    });
     const db = client.db("node-restapi");
     console.log("db is connected");
     return db;
