b533c73f55ca5a8d8ffcbbe39a14054894bee685
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-03-14 19:30:53 -0600

    I modified the database connection string

--- public/database.js
+++ public/database.js
@@ -2,9 +2,12 @@ const MongoClient = require("mongodb").MongoClient;
 
 async function connect() {
   try {
-    const client = await MongoClient.connect("mongodb://localhost:27017/", {
-      useUnifiedTopology: true,
-    });
+    const client = await MongoClient.connect(
+      "mongodb+srv://generalRank:Rufetty2%2E@cluster0.37ghr.mongodb.net/firstapi?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+      }
+    );
     const db = client.db("node-restapi");
     console.log("db is connected");
     return db;
