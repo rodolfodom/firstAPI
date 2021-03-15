60f91275cbc26d547d74356fab45ac4dc148e7a4
rodolfodom <rodolfodominguez60@aragon.unam.mx>
2021-03-15 09:11:40 -0600

    I added dotenv and cross-env dependencies

--- public/database.js
+++ public/database.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const MongoClient = require("mongodb").MongoClient;
 
 async function connect() {
--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
+require("dotenv").config();
 const app = require("./server.js");
 const connect = require("./database.js");
+
+console.log(process.env.NODE_ENV);
 
 async function main() {
   await app.listen(app.get("port"));
